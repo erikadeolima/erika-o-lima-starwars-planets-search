75d5b60fd0ff581f80b282ef422160d2e1aba1d4
Erika de Oliveira Lima <99990418+erikadeolima@users.noreply.github.com>
2022-07-16 17:35:20 -0300

    Update Api.js

--- src/hooks/Api.js
+++ src/hooks/Api.js
@@ -1,12 +1,12 @@
 import testData from '../mock/testData';
 
 const useApi = async () => {
-  /* const response = await fetch('https://swapi-trybe.herokuapp.com/api/planets/');
+  const response = await fetch('https://swapi-trybe.herokuapp.com/api/planets/');
   const json = await response.json();
-  const { results } = json; */
-  const json = testData;
   const { results } = json;
-  console.log('MOCK');
+  // const json = testData;
+  // const { results } = json;
+  // console.log('MOCK');
   return results;
 };
 
